9be1c1c945c3eb05c563b8bad5947a4b1d7f5cba
Uelli <wellison.fdsilva@gmail.com>
2024-11-25 08:05:01 -0300

    comit3

--- codigo/funcao.php
+++ codigo/funcao.php
@@ -16,4 +16,3 @@ function salvarCadastro($conexao, $nome, $endereco, $email, $telefone, $cpf)
     }
 }
 ?>
-
--- codigo/gravar.php
+++ codigo/gravar.php
@@ -1,14 +1,15 @@
 <?php
+require_once 'conexao.php';
+require_once "funcao.php";
+
 $nome = $_POST['nome'];
 $endereco = $_POST['endereco'];
 $email = $_POST['email'];
 $telefone = $_POST['telefone'];
 $cpf = $_POST['cpf'];
 
-require_once 'conexao.php';
-require_once "funcao.php";
 
 salvarCadastro($conexao, $nome, $endereco, $email, $telefone, $cpf);
-header('Location: index.html');
+header('Location: pagina2.html');
 exit;
 ?>
